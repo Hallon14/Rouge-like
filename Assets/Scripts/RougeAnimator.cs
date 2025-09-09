b8edf3b9504515c3cb22b9490b025caa29d9f28f
Hallon14 <ottowendt@yahoo.se>
2025-09-10 01:21:33 +0200

    Added more animations
    Somehow i fucky fucky the running animation but whatever

--- Assets/Scripts/RougeAnimator.cs
+++ Assets/Scripts/RougeAnimator.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class RougeAnimator : MonoBehaviour
@@ -5,12 +6,17 @@ public class RougeAnimator : MonoBehaviour
     [SerializeField] private PlayerController playercontroller;
     [SerializeField] private Animator animator;
 
+    private Vector3 direction;
+    private Rigidbody2D body;
+
     void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
-    private void FixedUpdate()
+    private void Update()
     {
+        direction = body.linearVelocity.normalized;
+
         //ANIMATIONS
         if (playercontroller.inputVector == new Vector2(0, 0))
         {
@@ -19,6 +25,23 @@ public class RougeAnimator : MonoBehaviour
         else
         {
             animator.SetBool("IsRunning", true);
+        }
+
+        if (direction.y < 0)
+        {
+            animator.SetBool("IsFalling", true);
+            animator.SetBool("IsJumping", false);
+        }
+        else if (direction.y > 0)
+        {
+            animator.SetBool("IsFalling", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+            
         }
     }
 }
