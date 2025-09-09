126e116f37652009a539c4b3e63a6868fa9009dc
Hallon14 <ottowendt@yahoo.se>
2025-09-09 10:10:11 +0200

    Created ParentPlayerClass

---
+++ Assets/Scripts/ParentPlayerClass.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ParentPlayerClass : MonoBehaviour
+{
+    private float MaxHeath;
+    private float MaxMana;
+    private float MoveSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
